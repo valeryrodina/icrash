851111f2400e1ced8b3ece469dd84f07f5fba3bc
Lera <Lera@LeraPC>
2016-09-22 16:27:57 +0300

    my first commit

---
+++ lu.uni.teaching.il2.tutorial.group11/src/lu/uni/teaching/il2/tutorial/group11/TutorialClass.java
@@ -0,0 +1,7 @@
+package lu.uni.teaching.il2.tutorial.group11;
+
+public class TutorialClass {
+	 public static void main(String[] args) {
+	        System.out.println("Hello World99999!");
+	    }
+}
